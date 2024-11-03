7c6a04b1363049d1dfbc77952dd7bff5763f5051
MOHAMMED SADIK K P <gibmepreo@gmail.com>
2024-11-03 13:06:50 +0530

    The Login Path imlimentation is completed with conditional state switch also

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
+import logo from '../../assets/logo.png';
 
 const Login = () => {
+    const [signState, setSignState] = useState('Sign In')
     return (
-        <div className='login'></div>
+        <div className='login'>
+            <img src={logo} className='login-logo' alt="" />
+            <div className="login-form">
+                <h1>{signState}</h1>
+                <form>
+                    {signState === 'Sign Up' ? <input type="text" placeholder='Your Name' /> : <></>}
+                    <input type="email" placeholder='Your Email' />
+                    <input type="password" placeholder='Your Passord' />
+                    <button className='submit-btn'>{signState}</button>
+                    <div className="form-help">
+                        <div className="remember">
+                            <input type="checkbox" />
+                            <label>Remember Me</label>
+                        </div>
+                        <p>Need Help?</p>
+                    </div>
+                </form>
+                <div className="form-switch">
+                    {signState === 'Sign Up' ? <p>Already have account? <span onClick={() => setSignState('Sign In')}>Sign In Now</span></p> : <p>New To Netflix? <span onClick={() => setSignState('Sign Up')}>Sign Up Now</span></p>}
+                </div>
+            </div>
+        </div>
     )
 }
 
