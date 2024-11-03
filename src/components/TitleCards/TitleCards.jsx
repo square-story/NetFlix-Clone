8820accaf19ea26c07c8f3c3335d003b1c0a527a
MOHAMMED SADIK K P <gibmepreo@gmail.com>
2024-11-03 18:08:58 +0530

    added the player and it functionality and did the navigater working

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -2,6 +2,7 @@
 import './TitleCards.css'
 import { useRef, useState } from 'react'
 import { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 
 const TitleCards = ({ title, category }) => {
@@ -36,10 +37,10 @@ const TitleCards = ({ title, category }) => {
             <h2>{title ? title : "Popular on Netflix"}</h2>
             <div className="card-list" ref={cardRef}>
                 {apiData.map((card, index) => {
-                    return <div className="card" key={index}>
+                    return <Link to={`/player/${card.id}`} className="card" key={index}>
                         <img src={`https://image.tmdb.org/t/p/w500` + card.backdrop_path} alt="" />
                         <p>{card.original_title}</p>
-                    </div>
+                    </Link>
                 })}
             </div>
         </div>
--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
 import "./Player.css";
+import back_arrow_icon from '../../assets/back_arrow_icon.png'
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
 const Player = () => {
+    const { id } = useParams();
+    const navigate = useNavigate();
+    const [apiData, setApiData] = useState({
+        name: '',
+        key: '',
+        published_at: '',
+        type: ''
+    })
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NWVjODJlNDRjOTdhOTBmM2NmNGRmZmU5Y2U5YmMyNiIsIm5iZiI6MTczMDYyODQ0OC42MDAzNjksInN1YiI6IjY3Mjc0YTFkNWFmMjllODg1NTNhZmIxZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.2uTx-2ORmcWUoB9ZtG0M8Er3afXXKeO6ZiDSxjn-_eo'
+        }
+    };
+
+    useEffect(() => {
+        fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`, options)
+            .then(res => res.json())
+            .then(res => setApiData(res.results[0]))
+            .catch(err => console.error(err));
+    }, [])
     return (
-        <div className='player'></div>
+        <div className='player'>
+            <img src={back_arrow_icon} alt="" onClick={() => navigate(-2)} />
+            <iframe width='90%' height='90%' src={`https://www.youtube.com/embed/${apiData.key}`} title="trailer" frameBorder='0' allowFullScreen ></iframe>
+            <div className="player-info">
+                <p>{apiData.published_at.slice(0, 10)}</p>
+                <p>{apiData.name}</p>
+                <p>{apiData.type}</p>
+            </div>
+        </div>
     )
 }
 
